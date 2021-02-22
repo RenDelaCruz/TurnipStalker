fe9e0fa2b69f1cc2917992394d8563c6f194835a
Ren Dela Cruz <ren_delacruz@outlook.com>
2021-02-22 01:01:44 -0500

    Added sticky button to top

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ $(document).ready(function () {
     });
 });
 
+$(document).on("click", "#myBtn", function () {
+    scrollTo("#top");
+});
+
 $(document).on("click", "div[id^='post-']", function () {
     var num = this.id.split('-')[1];
     scrollTo('#post-' + num);
@@ -251,3 +255,23 @@ function getTimeAgo(ts) {
     return seconds + " seconds ago";
 }
 
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+/*
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}*/
